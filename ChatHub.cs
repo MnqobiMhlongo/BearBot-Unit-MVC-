5af5e37bb4b6d50f997df661cd7b51bbe72bb778
MnqobiMhlongo <nqobimhlongo86@gmail.com>
2024-11-08 14:16:12 +0200

    Add project files.

---
+++ ChatHub.cs
@@ -0,0 +1,47 @@
+﻿using GenerativeAI.Models;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace signalRxGemini
+{
+    public class ChatHub : Hub
+    {
+        public void Send(String name, string message )
+        {
+            Clients.All.addNewMessageToPage(name, message);
+            if(name!= "Bear Bot")
+            {
+                bearBot(message);
+            }
+        }
+
+        public async Task bearBot(string message)
+        {
+            var apiKey = "AIzaSyAtsBMgDdIZim-K4Bp_51SPkN6m61rP0bk";
+
+            var model = new GenerativeModel(apiKey);
+          
+
+            var res = await model.GenerateContentAsync(message);
+
+            if(res == null)
+            {
+                bearBot(message);
+            }
+            else
+            {
+                Send("Bear Bot", res.ToString());
+            }
+
+            
+        }
+
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(signalRxGemini.Startup))]
+namespace signalRxGemini
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
